2b78c5e4748cc404eb85a4b35e891f3477e36a0e
GraffiasAntares <star211@list.ru>
2023-11-02 18:27:33 +0700

    basic functions

---
+++ main.py
@@ -0,0 +1,27 @@
+# Алгоритм задания
+def algorythm(row, matrix):
+    pass
+
+
+# Ввод матрицы вручную
+def matrix_input(row, column):
+    pass
+
+
+# Генерирование матрицы
+def matrix_generation(row, column):
+    pass
+
+
+# Вывод матрицы
+def show_matrix(matrix, n, m):
+    pass
+
+
+def main():
+    # Здесь будет прописано меню
+    pass
+
+
+if __name__ == "__main__":
+    main()
